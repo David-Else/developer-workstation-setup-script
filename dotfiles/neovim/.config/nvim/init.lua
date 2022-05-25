bde08075f19d63a87d6c5c56b97f320c6655c4d9
David <12832280+David-Else@users.noreply.github.com>
2022-05-25 09:50:53 +0100

    Add jsonls with schemas

--- dotfiles/neovim/.config/nvim/init.lua
+++ dotfiles/neovim/.config/nvim/init.lua
@@ -454,6 +454,37 @@ lspconfig.ltex.setup {
   },
 }
 
+lspconfig.jsonls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  settings = {
+    json = {
+      schemas = {
+        {
+          fileMatch = { 'package.json' },
+          url = 'https://json.schemastore.org/package.json',
+        },
+        {
+          fileMatch = { 'tsconfig.json', 'tsconfig.*.json' },
+          url = 'http://json.schemastore.org/tsconfig',
+        },
+        {
+          fileMatch = { '.eslintrc.json', '.eslintrc' },
+          url = 'http://json.schemastore.org/eslintrc',
+        },
+        {
+          fileMatch = { '.prettierrc', '.prettierrc.json', 'prettier.config.json' },
+          url = 'http://json.schemastore.org/prettierrc',
+        },
+        {
+          fileMatch = { 'deno.json' },
+          url = 'https://raw.githubusercontent.com/denoland/deno/main/cli/schemas/config-file.v1.json',
+        },
+      },
+    },
+  },
+}
+
 -- ==================
 --    null-ls.nvim
 -- ==================
