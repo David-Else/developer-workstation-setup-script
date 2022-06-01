81357c724d8ddb55ac2a900887a03d59869beaa7
David <12832280+David-Else@users.noreply.github.com>
2022-06-01 19:24:26 +0100

    Fix diagnostics toggle

--- dotfiles/neovim/.config/nvim/init.lua
+++ dotfiles/neovim/.config/nvim/init.lua
@@ -124,8 +124,8 @@ local diagnostics_active = true
 vim.keymap.set('n', '<leader>d', function()
   diagnostics_active = not diagnostics_active
   if diagnostics_active then
-    vim.diagnostic.show()
+    vim.diagnostic.enable()
   else
-    vim.diagnostic.hide()
+    vim.diagnostic.disable()
   end
 end)
