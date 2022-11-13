adf51bfb6ca8716a8345a155e8a3d60cfd220225
David-Else <12832280+David-Else@users.noreply.github.com>
2022-11-13 12:54:34 +0000

    Add helix language file config

--- install-rust-helix.sh
+++ install-rust-helix.sh
@@ -12,6 +12,7 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 SOURCE_DIR=~/src/helix                # directory to store helix source, user changeable
 CONFIG_DIR=~/.config/helix            # default, don't change
 CONFIG_FILE="$CONFIG_DIR/config.toml" # default, don't change
+LANG_FILE="$CONFIG_DIR/languages.toml"
 
 mkdir -p $SOURCE_DIR
 git clone https://github.com/helix-editor/helix $SOURCE_DIR
@@ -45,11 +46,81 @@ insert = "bar" # change cursor shape in insert mode
 [editor.file-picker]
 hidden = false # don't ignore hidden files
 EOF
+
     echo "
     $CONFIG_FILE created"
 else
     echo "
     $CONFIG_FILE exists, skipping creating new one"
+fi
+
+if [ ! -f $LANG_FILE ]; then
+    cat >$LANG_FILE <<EOF
+[[language]]
+name = "toml"
+config = {}
+auto-format = true
+
+[[language]]
+name = "html"
+formatter = { command = 'prettier', args = ["--parser", "html"] }
+
+[[language]]
+name = "css"
+formatter = { command = 'prettier', args = ["--parser", "css"] }
+auto-format = true
+
+[[language]]
+name = "markdown"
+# formatter = { command = 'prettier', args = ["--parser", "markdown"] }
+formatter = { command = 'prettier', args = [
+  "--parser",
+  "markdown",
+  "--prose-wrap",
+  "always",
+] }
+auto-format = true
+
+[[language]]
+name = "bash"
+formatter = { command = 'shfmt', args = ["-i", "4"] }
+auto-format = true
+
+[[language]]
+name = "rust"
+[language.config]
+cargo = { allFeatures = true }
+checkOnSave = { command = "clippy" }
+
+[[language]]
+name = "json"
+formatter = { command = 'deno', args = ["fmt", "-", "--ext", "json"] }
+
+[[language]]
+name = "javascript"
+formatter = { command = 'deno', args = ["fmt", "-", "--ext", "js"] }
+auto-format = true
+
+[[language]]
+name = "typescript"
+formatter = { command = 'deno', args = ["fmt", "-", "--ext", "ts"] }
+auto-format = true
+
+[[language]]
+name = "jsx"
+formatter = { command = 'deno', args = ["fmt", "-", "--ext", "jsx"] }
+auto-format = true
+
+[[language]]
+name = "tsx"
+formatter = { command = 'deno', args = ["fmt", "-", "--ext", "tsx"] }
+auto-format = true
+EOF
+    echo "
+    $LANG_FILE created"
+else
+    echo "
+    $LANG_FILE exists, skipping creating new one"
 fi
 
 display_text "
@@ -58,7 +129,7 @@ ${BOLD}Finished!${RESET}
 Helix has been installed and compiled from source
 
 - Don't delete the source directory: $SOURCE_DIR
-- Make configuration changes in: $CONFIG_DIR/config.toml
+- Make configuration changes in: $CONFIG_DIR/
 - Update with:
 
 cd $SOURCE_DIR
