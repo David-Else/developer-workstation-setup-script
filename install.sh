fb9d2fc14686653e0d77be05ad906a6af76dc70d
David-Else <12832280+David-Else@users.noreply.github.com>
2023-01-17 15:07:32 +0000

    Update NPM dependencies

--- install.sh
+++ install.sh
@@ -67,9 +67,9 @@ flathub_packages_to_install=(
     org.signal.Signal)
 
 npm_global_packages_to_install=(
-    bash-language-server@4.4.0
-    prettier@2.8.2
-    typescript-language-server@3.0.1
+    bash-language-server@4.5.0
+    prettier@2.8.3
+    typescript-language-server@3.0.2
     typescript@4.9.4
     vscode-langservers-extracted@4.5.0)
 
