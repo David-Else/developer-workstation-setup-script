d9a6a174a4aec14009722503e343ca3b80063a82
David-Else <12832280+David-Else@users.noreply.github.com>
2023-02-02 14:27:04 +0000

    Merge files

--- install-setup.bash
+++ install-setup.bash
@@ -4,9 +4,39 @@ set -euo pipefail
 source functions.bash
 confirm_user_is 'normal'
 
+GREEN="\e[38;5;46m"
+RESET="\e[0m"
 BIN_INSTALL_DIR=/usr/local/bin
 SOURCE_DIR=~/src/helix # helix source code directory
 TERMINAL=kitty         # terminal program to use for desktop integration
+
+#==============================================================================
+# Set host name
+#==============================================================================
+read -rp "What is this computer's name? [$HOSTNAME] " hostname
+if [[ ! -z "$hostname" ]]; then
+    hostnamectl set-hostname "$hostname"
+fi
+
+#==============================================================================
+# Fonts
+#==============================================================================
+read -p "Use sub-pixel rendering? (recommended for monitors with less than 4k resolution) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    add_to_file "$HOME/.Xresources" "Xft.lcdfilter: lcddefault"
+    dconf write /org/gnome/settings-daemon/plugins/xsettings/antialiasing "'rgba'"
+    echo "Sub-pixel rendering on"
+fi
+
+# create template file for nautilus
+touch "$HOME/Templates/text-file.txt"
+
+# set the protocol to use for git clone and push operations for GitHub CLI
+gh config set git_protocol ssh
+#==============================================================================
+# Install remaining binaries
+#==============================================================================
 
 # deno
 curl -fsSL https://deno.land/x/install/install.sh | sh
