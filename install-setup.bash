e8a6b4528249d80c581610b6fa1ba1e1a9710e13
David <12832280+David-Else@users.noreply.github.com>
2023-06-29 11:43:36 +0100

    Remove rclone binary download

--- install-setup.bash
+++ install-setup.bash
@@ -67,8 +67,8 @@ fi
 #==============================================================================
 curl -fsSL https://deno.land/x/install/install.sh | sh
 
-echo
-curl https://rclone.org/install.sh | sudo bash || true
+# echo
+# curl https://rclone.org/install.sh | sudo bash || true
 
 sudo curl -L https://github.com/lemnos/tt/releases/download/v0.4.2/tt-linux -o $bin_install_folder/tt && sudo chmod +x $bin_install_folder/tt
 sudo curl -o /usr/share/man/man1/tt.1.gz -L https://github.com/lemnos/tt/releases/download/v0.4.2/tt.1.gz
